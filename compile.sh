138a212cbc2aafdac867f6bd63ca9d4031bfac53
chLi <565353780@qq.com>
2024-11-29 17:55:17 +0800

    fix running bug

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 LD_LIBRARY_PATH=$HOME/github/AMCAX/point-cloud-reverse-algorithm/amcax-step-io/3rd/AMCAXKernel_Release/lib:$LD_LIBRARY_PATH
 
-# rm -rf build
+rm -rf build
 
 mkdir build
 cd build
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "io.h"
-#include <filesystem>
 #include <iostream>
 #include <string>
 
@@ -11,23 +10,19 @@ int main(int argc, char *argv[]) {
 
   // const std::string json_folder_path = "../bspline-fitting/output/";
   // const std::string save_step_folder_path = "../bspline-fitting/output/";
-  const std::string json_folder_path = argv[1];
-  const std::string save_step_folder_path = argv[2];
+  const std::string json_file_path = argv[1];
+  const std::string save_step_file_path = argv[2];
   const bool overwrite = true;
 
-  for (const auto &entry :
-       std::filesystem::recursive_directory_iterator(json_folder_path)) {
-    const std::filesystem::path file_path = entry.path();
+  std::cout << "start convert [" << json_file_path << "] --> ["
+            << save_step_file_path << "]" << std::endl;
+  if (!toStepFile(json_file_path, save_step_file_path, overwrite)) {
+    std::cerr << "\t convert failed!" << std::endl;
 
-    if (entry.is_regular_file() && file_path.extension() == ".json") {
-      const std::string save_step_file_path =
-          save_step_folder_path + file_path.stem().string() + ".step";
-
-      std::cout << "start convert [" << file_path.string() << "] --> ["
-                << save_step_file_path << "]" << std::endl;
-      toStepFile(file_path, save_step_file_path, overwrite);
-    }
+    return -1;
   }
+
+  std::cerr << "\t convert success!" << std::endl;
 
   return 1;
 }
