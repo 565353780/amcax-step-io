b7728f6ab3ba68a5063c11d82a260ec4bf35bc5f
chLi <565353780@qq.com>
2024-11-29 10:30:09 +0800

    update package name

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-LD_LIBRARY_PATH=$HOME/github/AMCAX/point-cloud-reverse-algorithm/amcax-trans/3rd/AMCAXKernel_Release/lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$HOME/github/AMCAX/point-cloud-reverse-algorithm/amcax-step-io/3rd/AMCAXKernel_Release/lib:$LD_LIBRARY_PATH
 
-./build/AMCAXTrans
+./build/AMCAXStepIO
